a7d75d3b5c5f3d7baa946b4dfdaa6eea90764222
Tommy Issa <tommyissa@hotmail.com>
2025-05-14 15:18:20 +0200

    Har skapat server-fil samt index-vy

---
+++ server.js
@@ -0,0 +1,39 @@
+let express = require('express');
+let { Client } = require('pg');
+require('dotenv').config();
+
+let app = express();
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+// Anslut till databasen
+let client = new Client({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    ssl: {
+        rejectUnauthorized: false,
+    }
+});
+
+client.connect((err) => {
+    if(err) {
+        console.log('Fel vid anslutnin: ' + err);
+    } else {
+        console.log('Ansluten till databasen');
+    }
+});
+
+// Routing
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+
+// Starta servern
+app.listen(process.env.PORT, () => {
+    console.log('Servern startas på port: ' + process.env.PORT);
+});
