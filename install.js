084d1abb5077aea9b96f4a10bd297b379ce9a113
Tommy Issa <tommyissa@hotmail.com>
2025-05-14 14:35:31 +0200

    Har skapat install-fil för att ansluta till databasen och skapa en tabell för kurser

---
+++ install.js
@@ -0,0 +1,35 @@
+let { Client } = require('pg');
+require('dotenv').config();
+
+// Anslut till databasen
+let client = new Client({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    ssl: {
+        rejectUnauthorized: false,
+    }
+});
+
+client.connect((err) => {
+    if(err) {
+        console.log('Fel vid anslutnin: ' + err);
+    } else {
+        console.log('Ansluten till databasen');
+    }
+});
+
+// Skapar kurstabell
+client.query(`
+    DROP TABLE IF EXISTS courses;
+    CREATE TABLE courses(
+        id SERIAL PRIMARY KEY,
+        coursecode TEXT,
+        coursename TEXT,
+        syllabus TEXT, 
+        progression TEXT,
+        added_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );    
+`);
